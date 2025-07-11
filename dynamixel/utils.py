bd8b1a5abd2e505c7739fe1e28b0f2865bcd011e
Derek Daniels <dbdaniel@mtu.edu>
2025-07-10 20:52:44 -0400

    [XL430_W250_T/utils] fix negative value conversion and move to utils

--- dynamixel/utils.py
+++ dynamixel/utils.py
@@ -12,3 +12,29 @@ class Lock:
 
     def __exit__(self, *args):
         self.locked = False
+
+
+def twosComplement(value, length):
+    """Compute the 2's complement of int value
+
+    Given an unsigned integer return signed value.
+
+    Would be better to do this with [from/to]_bytes but
+    the "signed" arg is not implemented in circuitpython ¯\_(ツ)_/¯
+
+    :param value: Unsigned integer
+    :type value: int
+    :param length: Length of unsigned integer in bytes
+    :type length: int
+    :returns: Signed integer
+    :rtype: int
+    """
+    maxInt = int.from_bytes(bytes([0xFF]*length), 'little')
+    if value < 0:
+        maxInt += value
+        return list(maxInt.to_bytes(length, 'little'))
+    width = length * 8
+    sign_bit = 1 << (width - 1)
+    if value & sign_bit:
+        value -= (1 << width)
+    return value
