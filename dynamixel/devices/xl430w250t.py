317893dd55d33ba579e1936492e07d6ac6204d0e
Derek Daniels <dbdaniel@mtu.edu>
2025-07-11 10:32:06 -0400

    [XL430_W250_T] standard returns and hints

--- dynamixel/devices/xl430w250t.py
+++ dynamixel/devices/xl430w250t.py
@@ -1,4 +1,4 @@
-from dynamixel.protocol import Protocol2
+from dynamixel.protocol import Protocol2, Response
 from dynamixel.servo import Servo, paramUnit
 from dynamixel import utils
 import asyncio
@@ -73,40 +73,40 @@ class XL430_W250_T(Servo):
     def __init__(self, *args, unit=PARAM_UNIT.UNIT_DEGREE, **kwargs):
         super(XL430_W250_T, self).__init__(*args, **kwargs)
         self.unit = unit
-        self.protocol = Protocol2(**kwargs)
+        self.protocol: Protocol2 = Protocol2(**kwargs)
         self.resolution = 4096
         self.torqueEnabled = False
         self.moving = False
+        self.presentPosition = 0
 
     async def run(self):
         while True:
-            self.getPresentPosition()
+            res = self.getPresentPosition()
+            if (res := self.getPresentPosition()).ok:
+                self.presentPosition = res.data
 
-            self.moving = bool(self.readControlTableItem(self.CONTROL_TABLE.MOVING))
-            self.torqueEnabled = bool(
-                self.readControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE)
-            )
+            if (res := self.readControlTableItem(self.CONTROL_TABLE.MOVING)).ok:
+                self.moving = bool(res.data)
+
+            if (res := self.readControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE)).ok:
+                self.torqueEnabled = bool(res.data)
             await asyncio.sleep(0.1)
 
-    def clear(self):
-        self.protocol.reboot(self.id)
+    def clear(self, position: bool = False, error: bool = False):
+        self.protocol.clear(self.id, position=position, error=error)
 
     def setProtocolVersion(self, protocol):
-        return self.write(Message.SET_PROTOCOL, protocol)
+        return self.writeControlTableItem(self.CONTROL_TABLE.PROTOCOL_TYPE, protocol)
 
     def ping(self):
         res = self.protocol.ping(self.id)
         return res
 
-    def torqueOn(self):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE, 1)
-        if res != "OK":
-            return res
+    def torqueOn(self) -> Response:
+        return self.writeControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE, 1)
 
     def torqueOff(self):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE, 0)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE, 0)
 
     def getBaud(self):
         res = self.readControlTableItem(self.CONTROL_TABLE.BAUD)
@@ -116,29 +116,27 @@ class XL430_W250_T(Servo):
         res = self.readControlTableItem(self.CONTROL_TABLE.BAUD)
         return res
 
-    def setBaudrate(self, value):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.BAUD, value)
-        if res != "OK":
-            return res
+    def setBaudrate(self, baud: int):
+        return self.writeControlTableItem(self.CONTROL_TABLE.BAUD, baud)
 
     def ledOn(self):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.LED, 1)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.LED, 1)
 
     def ledOff(self):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.LED, 0)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.LED, 0)
 
     def setOperationMode(self, operatingMode):
-        res = self.writeControlTableItem(
+        return self.writeControlTableItem(
             self.CONTROL_TABLE.OPERATING_MODE, operatingMode
         )
-        if res != "OK":
-            return res
 
-    def convertNegative(self, value, length):
+    def convertToNegative(self, value, length):
+        if value < 0:
+            maxInt = int.from_bytes(bytes([0xFF] * length), "little")
+            maxInt += value
+            return list(maxInt.to_bytes(length, "little"))
+
+    def convertFromNegative(self, value, length):
         return utils.twosComplement(value, length)
 
     def setGoalPosition(self, value, unit=None):
@@ -146,33 +144,29 @@ class XL430_W250_T(Servo):
         unit = unit or self.unit
         value = self.convertUnits(value, unit)
         if value < 0:
-            value = self.convertNegative(value, self.CONTROL_TABLE.GOAL_POSITION[1])
-        res = self.writeControlTableItem(self.CONTROL_TABLE.GOAL_POSITION, value)
-        if res != "OK":
-            return res
+            value = self.convertToNegative(value, self.CONTROL_TABLE.GOAL_POSITION[1])
+        return self.writeControlTableItem(self.CONTROL_TABLE.GOAL_POSITION, value)
 
     def getPresentPosition(self, unit=None):
         unit = unit or self.unit
         res = self.readControlTableItem(self.CONTROL_TABLE.PRESENT_POSITION)
-        if isinstance(res, int):
-            res = self.convertNegative(res, self.CONTROL_TABLE.GOAL_POSITION[1])
-            res = self.convertRaw(res, unit)
-            self.position = res
-        return self.position
+        data = None
+        if res.ok:
+            data = self.convertFromNegative(
+                res.data, self.CONTROL_TABLE.GOAL_POSITION[1]
+            )
+            data = self.convertRaw(data, unit)
+        return Response(data, res.err)
 
     def setMaxPosition(self, value, unit=None):
         unit = unit or self.unit
         value = self.convertUnits(value, unit)
-        res = self.writeControlTableItem(self.CONTROL_TABLE.MAX_POSITION_LIMIT, value)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.MAX_POSITION_LIMIT, value)
 
     def setMinPosition(self, value, unit=None):
         unit = unit or self.unit
         value = self.convertUnits(value, unit)
-        res = self.writeControlTableItem(self.CONTROL_TABLE.MIN_POSITION_LIMIT, value)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.MIN_POSITION_LIMIT, value)
 
     def getPositionLimits(self, unit=None):
         unit = unit or self.unit
@@ -185,23 +179,16 @@ class XL430_W250_T(Servo):
         return (minRes, maxRes)
 
     def setGoalVelocity(self, value):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.GOAL_VELOCITY, value)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.GOAL_VELOCITY, value)
 
     def getPresentVelocity(self):
-        res = self.readControlTableItem(self.CONTROL_TABLE.PRESENT_VELOCITY)
-        return res
+        return self.readControlTableItem(self.CONTROL_TABLE.PRESENT_VELOCITY)
 
     def setGoalPwm(self, value):
-        res = self.writeControlTableItem(self.CONTROL_TABLE.GOAL_PWM, value)
-        if res != "OK":
-            return res
+        return self.writeControlTableItem(self.CONTROL_TABLE.GOAL_PWM, value)
 
     def getPresentPwm(self):
-        res = self.readControlTableItem(self.CONTROL_TABLE.PRESENT_PWM)
-        return res
+        return self.readControlTableItem(self.CONTROL_TABLE.PRESENT_PWM)
 
     def getTorqueEnabled(self):
-        res = self.readControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE)
-        return res
+        return self.readControlTableItem(self.CONTROL_TABLE.TORQUE_ENABLE)
